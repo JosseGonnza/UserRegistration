b22e754f9dfa36efafca4b24f4b3aaef597ded4d
JosseGonnza <accion.quevedo@hotmail.com>
2024-06-30 12:11:34 +0100

    First Commit!

---
+++ src/main/java/org/jossegonnza/UserRegistrationApp.java
@@ -0,0 +1,13 @@
+package org.jossegonnza;
+
+/**
+ * Hello world!
+ *
+ */
+public class UserRegistrationApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
