2924999a8a7afda81e654d9565fb015d2617623f
JosseGonnza <accion.quevedo@hotmail.com>
2024-07-02 09:01:00 +0100

    Dead end Street.

--- src/main/java/org/jossegonnza/RegisterUser.java
+++ src/main/java/org/jossegonnza/RegisterUser.java
@@ -1,15 +1,18 @@
 package org.jossegonnza;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import java.util.regex.Pattern;
 
 public class RegisterUser {
 
+    List<String> emailsList = new ArrayList<>();
+
     public User register(String email, String password) {
         String userId = String.valueOf(UUID.randomUUID());
 
-        isValidPassword(password);
-        isValidEmail(email);
+        userValidator(email, password);
 
         return new User(userId, email);
     }
@@ -18,12 +21,31 @@ public class RegisterUser {
         if (password.length() > 8 && password.contains("_")) {
             return true;
         }
-        return false;
+        throw new IllegalArgumentException("Password does not meet security requirements.");
     }
 
     public boolean isValidEmail(String email) {
         String emailRegex = "^[A-Za-z0-9-_.]+@[A-Za-z0-9.-]+$";
         Pattern pattern = Pattern.compile(emailRegex);
-        return pattern.matcher(email).matches();
+
+        if (pattern.matcher(email).matches()) {
+            return true;
+        }
+        throw new IllegalArgumentException("Invalid email format.");
+    }
+
+    public boolean isUniqueEmail(String email) {
+        emailsList.add(email);
+        if (!emailsList.contains(email)) {
+            return true;
+        }
+        throw new IllegalArgumentException("Email is already in use.");
+    }
+
+    public boolean userValidator(String email, String password) {
+        if (isValidPassword(password) && isValidEmail(email) && isUniqueEmail(email)) {
+            return true;
+        }
+        throw new IllegalArgumentException("Is not possible create user.");
     }
 }
--- src/test/java/org/jossegonnza/RegisterUserTest.java
+++ src/test/java/org/jossegonnza/RegisterUserTest.java
@@ -3,6 +3,10 @@ package org.jossegonnza;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+
 class RegisterUserTest {
 
     @Test
@@ -26,20 +30,6 @@ class RegisterUserTest {
     public void should_contained_at_least_8_characters() {
         //Given
         RegisterUser registerUser = new RegisterUser();
-        String password = "Pass";
-        String email = "test@example.com";
-
-        //When
-        User user = registerUser.register(email, password);
-
-        //Then
-        Assertions.assertThat(registerUser.isValidPassword(password)).isFalse();
-    }
-
-    @Test
-    public void should_contained_an_underscore() {
-        //Given
-        RegisterUser registerUser = new RegisterUser();
         String password = "Password_";
         String email = "test@example.com";
 
@@ -48,6 +38,39 @@ class RegisterUserTest {
 
         //Then
         Assertions.assertThat(registerUser.isValidPassword(password)).isTrue();
+
+        assertThatThrownBy(() -> {
+            registerUser.userValidator(email, password);
+        }).hasMessage("Password does not meet security requirements.");
+
+
+//        try {
+//            registerUser.isValidPassword("Pass_");
+//        } catch (IllegalArgumentException e) {
+//            Assertions.assertThat(e.getMessage().contains("Password does not meet security requirements.")).isTrue();
+//        }
+
+        //        Assertions.assertThat(registerUser.isValidPassword(password)).isFalse();
+    }
+
+    @Test
+    public void should_contained_an_underscore() {
+        //Given
+        RegisterUser registerUser = new RegisterUser();
+        String password = "Password";
+        String email = "test@example.com";
+
+        //When
+        User user = registerUser.register(email, password);
+
+        //Then
+        try {
+            registerUser.isValidPassword("Password");
+        } catch (IllegalArgumentException e) {
+            Assertions.assertThat(e.getMessage().contains("Password does not meet security requirements.")).isTrue();
+        }
+
+        //        Assertions.assertThat(registerUser.isValidPassword(password)).isTrue();
     }
 
     @Test
@@ -55,12 +78,43 @@ class RegisterUserTest {
         //Given
         RegisterUser registerUser = new RegisterUser();
         String password = "Password_";
-        String email = "test@example.com";
+        String email = "test_example.com";
 
         //When
         User user = registerUser.register(email, password);
 
         //Then
-        Assertions.assertThat(registerUser.isValidEmail(email)).isTrue();
+        try {
+            registerUser.isValidEmail("test_exmaple.com");
+        } catch (IllegalArgumentException e) {
+            Assertions.assertThat(e.getMessage().contains("Invalid email format.")).isTrue();
+        }
+
+        //        Assertions.assertThat(registerUser.isValidEmail(email)).isTrue();
+    }
+
+    @Test
+    public void should_be_an_unique_email() {
+        //Given
+        RegisterUser registerUser = new RegisterUser();
+        String password_test1 = "Password_";
+        String email_test1 = "test@exmaple.com";
+        String password_test2 = "Password_2";
+        String email_test2 = "test@exmaple.com";
+
+        //When
+        User user_test1 = registerUser.register(email_test1, password_test1);
+        User user_test2 = registerUser.register(email_test2, password_test2);
+
+        //Then
+        try {
+            registerUser.isUniqueEmail("test@exmaple.com");
+        } catch (IllegalArgumentException e) {
+            Assertions.assertThat(e.getMessage().contains("Email is already in use.")).isTrue();
+        }
+
+        //        Assertions.assertThat(user_test1.getEmail()).isNotEqualTo(user_test2.getEmail());
+
+
     }
 }
